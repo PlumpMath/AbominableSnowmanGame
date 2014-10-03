f74c3611d935ab5f6ecfbcb2919d6df89262689d
Tim Backus <backustm197@potsdam.edu>
2014-10-03 18:25:00 -0400

    Tim: > Removed old key bindings > Poll keys instead of executing a function on keypress > Reparent camera to player > A and D now rotate the player CCW/CW > W and S now move forward relative to the player's direction they are facing > Added constants for easy tweaking.

--- teamabomhim/BulletSnowman.py
+++ teamabomhim/BulletSnowman.py
@@ -1,18 +1,36 @@
 import direct.directbase.DirectStart
-from panda3d.core import Vec3, BitMask32, Point3, VBase3
+from panda3d.core import Vec3, BitMask32, Point3, KeyboardButton
 from panda3d.bullet import BulletWorld
 from panda3d.bullet import BulletRigidBodyNode, BulletDebugNode, BulletCharacterControllerNode
 from panda3d.bullet import BulletBoxShape, BulletCapsuleShape, BulletCylinderShape, BulletPlaneShape
 from panda3d.bullet import ZUp
+from math import sin, cos, pi
 from direct.showbase.DirectObject import DirectObject
 
-# Camera setup
-base.cam.setPos(8, -30, 8)
-base.cam.lookAt(0, 0, 0)
+# Constants
+DEBUG = True
+TURN_SPEED = 180
+DEG_TO_RAD = pi/180
+MAX_VEL_XY = 50
+MAX_VEL_Z = 5000
+GRAVITY = 25
+MOVE_SPEED = 500
+PNT = Point3(0,0,0)
+
+def debugP(message):
+	if(DEBUG):
+		print(message)
+
+# Keybindings
+keyForward = KeyboardButton.ascii_key('w')
+keyBack = KeyboardButton.ascii_key('s')
+keyLeft = KeyboardButton.ascii_key('a')
+keyRight = KeyboardButton.ascii_key('d')
+keySpace = KeyboardButton.ascii_key(' ')
  
 # World
 world = BulletWorld()
-world.setGravity(Vec3(0, 0, -50))
+world.setGravity(Vec3(0, 0, -GRAVITY))
 worldNP = render.attachNewNode('World')
 
 # This is actually really useful. It shows collision boxes and other info.
@@ -34,28 +52,29 @@ world.attachRigidBody(node)
 # Yeti
 yetiHeight = 7
 yetiRadius = 2
-
 yetiShape = BulletCapsuleShape(yetiRadius, yetiHeight - 2 * yetiRadius, ZUp)
-
 yetiModel = loader.loadModel("models/yeti")
-yetiModel.setH(-90)
+yetiModel.setH(90)
 yetiModel.setPos(0, 0, -1)
 yetiModel.flattenLight()
 
+# Type BulletRigidBodyNode
 playerNode = BulletRigidBodyNode("Player")
 playerNode.setMass(8.0)
 playerNode.addShape(yetiShape)
 playerNode.setAngularFactor(Vec3(0,0,0))
 playerNode.setDeactivationEnabled(False) # Without this disabled, things will weld together after a certain amount of time. It's really annoying.
-#playerNode.setLinearDamping(0.9) # We can use this for ice.
 
+# Type NodePath
 playerNP = worldNP.attachNewNode(playerNode)
 yetiModel.reparentTo(playerNP)
 playerNP.setPos(0, 0, 6)
 playerNP.setH(0)
-
 world.attachRigidBody(playerNP.node())
- 
+
+# Camera setup behind Yeti
+base.cam.setPos(0, -40, 10)
+base.cam.reparentTo(playerNP)
 
 # Press F1 to enable debug mode.
 def toggleDebug():
@@ -64,41 +83,60 @@ def toggleDebug():
   else:
     debugNP.hide()
 
-# Still working on this.
-def move(x, y, pressed):
-	if(pressed):
-		playerNode.applyForce(Vec3(x, y, 0), Point3(0,0,0))
-	else:
-		print("up")
-		playerNode.applyForce(Vec3(-x, -y, 0), Point3(0,0,0))
-
-# A jump of sorts...
-def doJump():
-	playerNode.applyForce(Vec3(0, 0, 24000), Point3(0,0,0))
-	
 world.setDebugNode(debugNP.node())
 debugNP.show()
 
-# Keybindings
+def move():
+	playerVelocity = playerNode.getLinearVelocity()
+	isJumping = False
+	playerRotation = playerNP.getH() 
+	
+	# TODO: Get rotation and move via camera angle
+	# We will need to do more math with the movement.
+	
+	# Function for polling keys
+	keyPressed = base.mouseWatcherNode.is_button_down
+	
+	# If we get a key hold, continuously apply force until we hit the max velocity
+	if keyPressed(keyForward):
+		debugP("forward")
+		if(abs(playerVelocity.getY()) > MAX_VEL_XY):
+			debugP("Y vel Capped")
+			playerVelocity.setY(MAX_VEL_XY)
+		else:
+			playerNode.applyForce((-MOVE_SPEED * sin(playerRotation * DEG_TO_RAD), MOVE_SPEED * cos(playerRotation * DEG_TO_RAD), 0), PNT)
+	elif keyPressed(keyBack):
+		debugP("back")
+		if(abs(playerVelocity.getY()) > MAX_VEL_XY):
+			debugP("Y vel at max")
+			playerVelocity.setY(-MAX_VEL_XY / 2)
+		else:
+			playerNode.applyForce((MOVE_SPEED * sin(playerRotation * DEG_TO_RAD) / 2, -MOVE_SPEED * cos(playerRotation * DEG_TO_RAD) / 2, 0), PNT)
+	else:
+		playerVelocity.setY(0) # Slow him down... somehow. This doesn't work yet.
+	
+	# Rotations
+	if keyPressed(keyLeft):
+		playerNP.setH(playerRotation + (globalClock.getDt() * TURN_SPEED))
+	elif keyPressed(keyRight):
+		playerNP.setH(playerRotation + (globalClock.getDt() * -TURN_SPEED))
+	
+	if keyPressed(keySpace):
+		debugP("jump")
+	
+	# Set the camera accordingly
+	#base.cam.setPos(playerNP.getX(), playerNP.getY() - 60, playerNP.getZ() + 10)
+	base.cam.lookAt(playerNP)
+
+# Function-based keybindings
 o = DirectObject()
 o.accept('f1', toggleDebug)
-o.accept('space', doJump)
-o.accept('w', move, [0, 8000, True])
-o.accept('s', move, [0, -8000, True])
-o.accept('a', move, [-8000, 0, True])
-o.accept('d', move, [8000, 0, True])
-o.accept('w-up', move, [0, 0, False])
-o.accept('s-up', move, [0, 0, False])
-o.accept('a-up', move, [0, 0, False])
-o.accept('d-up', move, [0, 0, False])
 
 # Update
 def update(task):
-
-	
-
 	dt = globalClock.getDt()
 	world.doPhysics(dt)
+	move()
 	return task.cont
  
 taskMgr.add(update, 'update')
