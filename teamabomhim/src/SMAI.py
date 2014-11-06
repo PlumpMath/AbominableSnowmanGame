8654951eeca6f863dc36e8edb73555629e720d4a
woodbt196 <woodbt196@potsdam.edu>
2014-11-05 23:37:24 -0800

    Made AI functional!

--- teamabomhim/src/SMAI.py
+++ teamabomhim/src/SMAI.py
@@ -1,5 +1,5 @@
 
-from panda3d.core import BitMask32, Vec3, Vec4, PNMImage, Filename, GeoMipTerrain, TextureStage, VBase4
+from panda3d.core import BitMask32, Vec3, Vec4, PNMImage, Filename, GeoMipTerrain, TextureStage, VBase4, Point3
 from panda3d.bullet import BulletWorld
 from panda3d.bullet import BulletRigidBodyNode, BulletDebugNode, BulletCharacterControllerNode, BulletGhostNode
 from panda3d.bullet import BulletBoxShape, BulletCapsuleShape, BulletCylinderShape, BulletPlaneShape, BulletHeightfieldShape
@@ -18,26 +18,31 @@ from SMBall import SMBall
 from SMGUI import SMGUI
 from SMGUIElement import SMGUIElement
 
+from math import sin, cos, pi
+
+#Globals for converting between radians
+DEG_TO_RAD = pi/180
+PNT = Point3(0,0,0)
 
 class SMAI():
-
+	#This constructs the nods tree needed for an AIChar object.
+	#Takes in a model as a string, a speed double, a world object, a bullet physics world and n x, y and z positions
 	def __init__(self, model, speed, world, worldNP, x, y, z):
-		self.AISpeed = speed
+		self.AISpeed = 80000
+		self.maxSpeed = speed
 		self.AIX = x
 		self.AIY = y
 		self.AIZ = z
 		self.worldNP = worldNP
 		bulletWorld = world
 		self.type = ""
-		# self.moveTo = False
 		self.AIModel = loader.loadModel(model)
-		self.AIModel.setScale(2)
-		# self.AIModel.setPos(x-15, y-15, z-15)
+		# self.AIModel.setScale(1)
 		self.AIModel.reparentTo(render)
 		
-		AIShape = BulletBoxShape(Vec3(0.5, 0.5, 0.5))
+		AIShape = BulletBoxShape(Vec3(2, 2, 2))
 		self.AINode = BulletRigidBodyNode('AIChar')
-		self.AINode.setMass(200)
+		self.AINode.setMass(100)
 		self.AINode.addShape(AIShape)
 		
 		self.AINode.setDeactivationEnabled(False)
@@ -48,8 +53,9 @@ class SMAI():
 		self.AIModel.reparentTo(self.AIChar)
 		self.AIChar.setPos(x, y, z)
 		bulletWorld.attachRigidBody(self.AIChar.node())
-		# return self.AIChar
 
+	#This method needs to be called on your AIChar object to determine what type of AI you want.
+	#Takes in a type string; either flee or seek and also a target object.
 	def setBehavior(self, type, target):
 		if(type == "flee"):
 			self.type = type
@@ -67,37 +73,35 @@ class SMAI():
 		self.target = target
 			
 	def flee(self):
-		# print("fleeing")
-		self.X0 = self.target.getX()
-		self.X1 = self.AIChar.getX()
-		self.Y0 = self.target.getY()
-		self.Y1 = self.AIChar.getY()
-		if(self.AIChar.getDistance(self.target) < 32.0):
-			# print("Flee!")
-			# print(Vec3.forward())
-			# self.AIChar.lookAt(self.target)
-			self.AIChar.setH(self.target.getH())
-			# self.AIChar.setAngularVelocity(self.AISpeed)
-			# self.AINode.setLinearVelocity(Vec3.forward())
-		
-		
+		if(self.AIChar.getDistance(self.target) < 40.0):
+			if(self.AINode.getLinearVelocity() > self.maxSpeed):
+				self.AIChar.setH(self.target.getH())
+				h = self.AIChar.getH()
+				hx = sin(h * DEG_TO_RAD) * 10
+				hy = cos(h * DEG_TO_RAD) * 10
+				self.AINode.applyForce(Vec3(-hx * self.AISpeed * globalClock.getDt(), hy * self.AISpeed * globalClock.getDt(), 0), PNT)
+
 	def seek(self):
-		print("seeking")
-		self.X0 = self.target.getX()
-		self.X1 = self.AIChar.getX()
-		self.Y0 = self.target.getY()
-		self.Y1 = self.AIChar.getY()
+		if(self.AIChar.getDistance(self.target) > 20.0):
+			if(self.AINode.getLinearVelocity() > self.maxSpeed):
+				self.AIChar.lookAt(self.target)
+				h = self.AIChar.getH()
+				hx = sin(h * DEG_TO_RAD) * 10
+				hy = cos(h * DEG_TO_RAD) * 10
+				self.AINode.applyForce(Vec3(-hx * self.AISpeed * globalClock.getDt(), hy * self.AISpeed * globalClock.getDt(), 0), PNT)
 		
 	def moveToRun(self):
+		self.seek()
 		# if(self.target.getX()+5 <= self.AIChar.getX() and self.target.getX()-5 >= self.AIChar.getX() and self.target.getY()+5 <= self.AIChar.getY() and self.target.getY()-5 >= self.AIChar.getY()):
-			print("It's stopped!")
+			# print("It's stopped!")
 			# self.AIChar.clearForces()
 			# self.AIChar.setLinearVelocity(0)
 		# else:
 			# self.AINode.clearForces()
 			# self.AIChar.lookAt(self.target)
 			# self.AINode.setLinearVelocity(self.AISpeed)
-		
+	
+	#Gets called on every AIChar in the world's update method to allow the AI to function at all.
 	def AIUpdate(self):
 		if(self.moveTo == True):
 			self.moveToRun()
